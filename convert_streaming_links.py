40747d27deb8c94b6a3462107ea309cc947bdb13
Bugsfree <113718082+bugsfreeweb@users.noreply.github.com>
2025-04-10 09:31:08 +0600

    Update convert_streaming_links.py

--- convert_streaming_links.py
+++ convert_streaming_links.py
@@ -4,12 +4,7 @@ from pathlib import Path
 import re
 
 # Define input sources
-SOURCES = [
-    "https://raw.githubusercontent.com/sydul104/main04/refs/heads/main/my",
-    "https://raw.githubusercontent.com/Miraz6755/Bdixtv/refs/heads/main/Livetv.m3u8",
-    "https://raw.githubusercontent.com/Yeadee/Toffee/refs/heads/main/toffee_ns_player.m3u",
-    "https://raw.githubusercontent.com/MohammadJoyChy/BDIXTV/refs/heads/main/Aynaott",
-    "https://raw.githubusercontent.com/Arunjunan20/My-IPTV/refs/heads/main/index.html",
+SOURCES = [    
     "https://aynaxpranto.vercel.app/files/playlist.m3u",
     "https://iptv-org.github.io/iptv/countries/us.m3u"
 ]
