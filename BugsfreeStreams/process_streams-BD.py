6ac51e141bdc42744e78567508e4edd395760ee3
Bugsfree <113718082+bugsfreeweb@users.noreply.github.com>
2025-04-14 14:34:14 +0600

    Update process_streams-BD.py

--- BugsfreeStreams/process_streams-BD.py
+++ BugsfreeStreams/process_streams-BD.py
@@ -18,7 +18,7 @@ MAX_STREAMS = 1000
 DEFAULT_LOGO = f"https://raw.githubusercontent.com/{REPO_OWNER}/{REPO_NAME}/refs/heads/{BRANCH}/BugsfreeLogo/default-logo.png"
 
 # Default single source
-# DEFAULT_SOURCE = "https://raw.githubusercontent.com/MohammadJoyChy/BDIXTV/refs/heads/main/Aynaott"
+DEFAULT_SOURCE = "https://aynaxpranto.vercel.app/files/playlist.m3u"
 
 # Static fallback M3U if all sources fail
 STATIC_M3U = """
