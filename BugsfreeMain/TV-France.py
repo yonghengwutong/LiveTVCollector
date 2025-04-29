4939d68da5828f51e0e55cd66e87c277b36fc58d
Bugsfree <113718082+bugsfreeweb@users.noreply.github.com>
2025-04-29 13:45:53 +0600

    Update TV-France.py

--- BugsfreeMain/TV-France.py
+++ BugsfreeMain/TV-France.py
@@ -258,6 +258,7 @@ def main():
     # Specific M3U sources (12 sources)
     source_urls = [
         "https://raw.githubusercontent.com/ipstreet312/freeiptv/refs/heads/master/all.m3u",
+        "https://raw.githubusercontent.com/HelmerLuzo/PlutoTV_HL/refs/heads/main/tv/m3u/PlutoTV_tv_FR.m3u",
         "https://iptv-org.github.io/iptv/countries/fr.m3u",
     ]
 
